97d04fa27102474056e000ecccbd88d8935c74f0
itisianpetts <itisianpetts@gmail.com>
2022-01-31 12:14:24 +0000

    'update login'

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,10 +11,18 @@ const Login = () => {
           <img src="/images/LI-Large.svg" alt="linkedin-logo" />
         </a>
         <div>
-          <Join>Join now</Join>
+          <JoinNow>Join now</JoinNow>
           <SignIn>Sign in</SignIn>
         </div>
       </Nav>
+      <Main>
+        <Section>
+          <Hero className="">
+            <h1>Welcome to your professional community</h1>
+          </Hero>
+          <StyledImage src="/images/LI-SideImage.svg" alt="" />
+        </Section>
+      </Main>
     </LoginContainer>
   );
 };
@@ -76,7 +84,7 @@ const Nav = styled.nav`
   }
 `;
 
-const Join = styled.a`
+const JoinNow = styled.a`
   font-size: 1rem;
   padding: 0 8px;
   line-height: 2rem;
@@ -84,7 +92,7 @@ const Join = styled.a`
   text-decoration: none;
   border-radius: 4px;
   color: rgba(0, 0, 0, 0.6);
-  transition: 0.3s ease-in-out;
+  transition: 0.2s ease-in-out;
   font-weight: 600;
   cursor: pointer;
   &:hover {
@@ -107,7 +115,51 @@ const SignIn = styled.a`
   padding: 0 1rem;
   text-align: center;
   text-decoration: none;
+  transition: 0.2s ease-in-out;
   &:hover {
     background-color: rgba(112, 181, 249, 0.15);
+    box-shadow: inset 0 0 0 2px #0a66c2;
+  }
+`;
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-wrap: nowrap;
+  position: relative;
+  min-height: 600px;
+  padding: 2.5rem 1rem 0 1rem;
+`;
+
+const StyledImage = styled.img`
+  width: 700px;
+  height: 560px;
+  flex-shrink: 1;
+  display: block;
+  z-index: -1;
+  position: relative;
+  outline: 0;
+`;
+
+const Hero = styled.div`
+  display: block;
+  align-self: flex-start;
+  box-sizing: border-box;
+  padding-right: 2.625rem;
+  flex-shrink: 0;
+  width: 55%;
+  position: relative;
+  > h1 {
+    color: rgba(143, 88, 73, 1);
+    font-size: 56px;
+    font-weight: 200;
+    outline: 0;
+    display: block;
+    line-height: 70px;
   }
 `;
